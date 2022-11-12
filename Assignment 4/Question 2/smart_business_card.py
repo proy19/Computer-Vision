e44774cec9a936692e2e7e196637ef1d1e8463aa
Priyangka R <57018537+proy19@users.noreply.github.com>
2022-11-12 14:07:12 -0500

    Add files via upload

---
+++ Assignment 4/Question 2/smart_business_card.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+camera_id = 0
+delay = 1
+window_name = 'Smart Business Card'
+
+qcd = cv2.QRCodeDetector()
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
+        if ret_qr:
+            for s, p in zip(decoded_info, points):
+                if s:
+                    print(s)
+                    color = (0, 255, 0)
+                else:
+                    color = (0, 0, 255)
+                frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
+                cv2.putText(frame, s, p[0].astype(int),
+                      cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.imshow(window_name, frame)
+
+    if cv2.waitKey(delay) & 0xFF == ord('q'):
+        break
+
+cv2.destroyWindow(window_name)
